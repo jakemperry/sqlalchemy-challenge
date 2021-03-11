e7de2cf987cd0193ea20049ce5881efea1afe334
jakemperry <Jakemperry@gmail.com>
2021-03-10 17:22:01 -0800

    list routes on home page

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+#Flask routes
+
+@app.route("/")
+def homepage():
+    return (
+        f"Available Routes: <br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
