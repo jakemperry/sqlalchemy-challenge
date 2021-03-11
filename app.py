a97627bf0ed432e3f357706fb0c7f2ca46289bbb
jakemperry <Jakemperry@gmail.com>
2021-03-10 17:27:48 -0800

    set up database connection and reference tables in app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Import python modules
 import numpy as np
 
 import sqlalchemy
@@ -7,6 +8,16 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+#Setup Database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#Setup Flask
 app = Flask(__name__)
 
 #Flask routes
@@ -21,3 +32,7 @@ def homepage():
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end>"
     )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
